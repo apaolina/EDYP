f3ef03d514fb59b7f6b4db876c64355c2ecf49b9
jcarzalo <jcarzalo@itba.edu.ar>
2023-04-14 20:33:42 -0300

    Update RestauranteV1.py

--- RestauranteV1.py
+++ RestauranteV1.py
@@ -64,3 +64,14 @@ class Menu:
             self.ingredientes.append(str(input("Ingrese los ingredientes del plato: ")))
             self.cant_ingredientes.append(int(input("Ingrese la cantidad del ingrediente: ")))
         self.tiempo_preparacion = int(input("Ingrese el tiempo de preparacion del plato: "))
+        
+#Se crea la clase Cliente
+class Clientes():
+    def __init__(self, Numero_cliente, Comensales):
+        self.Numero_cliente = Numero_cliente
+        self.Comensales = Comensales
+        self.Estado_Cliente = 'En espera de mesa'
+    def Asignar_mesa_cliente(self):
+        self.Estado_Cliente = 'Mesa asignada'
+    def Desasginar_mesa_cliente(self):
+        self.Estado_Cliente = 'Termino'
