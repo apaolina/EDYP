1992329ae6dbdc790dca303395a2955511afb685
apaolina <128314554+apaolina@users.noreply.github.com>
2023-04-29 18:19:42 -0300

    Cambio GUI y HASHEO

--- GUI.py
+++ GUI.py
@@ -1,58 +1,14 @@
-from tkinter import *
-from tkinter import messagebox
-import hashlib as hash
 from Hasheo import *
 
-root = Tk()
-root.title("Inicio de sesión")
-
-Label(root, text="Usuario").grid(row=0)
-Label(root, text="Contraseña").grid(row=1)
-
-username_entry = Entry(root)
-password_entry = Entry(root, show="*")
-
-username_entry.grid(row=0, column=1)
-password_entry.grid(row=1, column=1)
-
-def create_account():
-    create_window = Toplevel(root)
-    create_window.title("Crear cuenta")
-
-    Label(create_window, text="Nombre de usuario").grid(row=0)
-    Label(create_window, text="Contraseña").grid(row=1)
-
-    new_username_entry = Entry(create_window)
-    new_password_entry = Entry(create_window, show="*")
-
-    
-    new_username_entry.grid(row=0, column=1)
-    new_password_entry.grid(row=1, column=1)
-
-    
-    def submit():
-        new_username = new_username_entry.get()
-        new_password = hash_password(new_password_entry.get())
-        with open("users.txt", "a") as file:
-            file.write(f"{new_username},{new_password}\n")
-        messagebox.showinfo("Cuenta creada", "La cuenta ha sido creada con éxito")
-        create_window.destroy()
-    Button(create_window, text="Crear cuenta", command=submit).grid(row=3)
-    
-def login():
-    username = username_entry.get()
-    password = password_entry.get().encode()
-    stored_password = None
-    stored_username = None
-    with open("users.txt", "r") as file:
-        for line in file.readlines():
-            try:
-                stored_username, stored_password = line.strip().split(",")
-            except  ValueError:
-                print("Error en el archivo de usuarios")
-            if (username) == stored_username and verify_password(stored_password, password) == stored_password:
-                messagebox.showinfo("Inicio de sesión exitoso", "Bienvenido!")
-                root.destroy()
-                return
-    messagebox.showerror("Error", "Nombre de usuario o contraseña incorrectos")
+def consola():
+    print("Bienvenido al simulador 'Restaurant City'")
+    print("1 - Iniciar sesion")
+    print("2 - Registrar nuevo usuario")
+    print("Ingrese el numero de la opcion que desea realizar:")
+    opcion = input()
+    if opcion == int:
+        pass
+    else:
+        print("Ingrese una opcion valida")
+        consola()
 
--- Hasheo.py
+++ Hasheo.py
@@ -1,19 +1,18 @@
 import hashlib
 import os
 
-def hash_password(password):
-    # Generar una sal aleatoria
-    salt = os.urandom(16)
-    # Aplicar el hash a la contraseña junto con la sal
-    hashed_password = hashlib.pbkdf2_hmac("sha256", password.encode(), salt, 100000)
-    # Devolver la sal y el hash de la contraseña
-    return salt + hashed_password
+def hashear_contraseña(contraseña):
+    inicio = os.urandom(16)
+    contraseña_hasheada = hashlib.pbkdf2_hmac("sha256", contraseña.encode(), inicio, 100000)
+    return inicio, contraseña_hasheada
 
-def verify_password(stored_password, entered_password):
-    # Obtener la sal y el hash de la contraseña almacenada
-    salt = stored_password[:16]
-    stored_hash = stored_password[16:]
-    # Aplicar el hash a la contraseña ingresada junto con la sal
-    entered_hash = hashlib.pbkdf2_hmac("sha256", entered_password, salt, 100000)
-    # Comparar los hashes
-    return entered_hash == stored_hash
+def verificar_contraseña(contraseña_almacenada, contraseña_ingresada, usuario_ingresado):
+    with open("inicio.txt", "r") as file:
+        for line in file.readlines():
+            inicio = line
+    with open("users.txt", "r") as file:
+        for line in file.readlines():
+            usuario_almacenado, contraseña_almacenada = line.strip().split(",")
+    if contraseña_almacenada == hashlib.pbkdf2_hmac("sha256", contraseña_ingresada.encode(), inicio, 100000) and usuario_almacenado == usuario_ingresado:
+        return True
+    
