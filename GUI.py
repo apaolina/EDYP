0c93288cc716d0cb2a12315c084b25afc1b9097d
apaolina <128314554+apaolina@users.noreply.github.com>
2023-04-29 19:51:16 -0300

    Dios santo que dificil es hashear

--- GUI.py
+++ GUI.py
@@ -1,39 +1,56 @@
 from Hasheo import *
 
+#Va a haber que agregar muchos verificadores acá. Especialmente cuando interactuamos con el users.txt
+
 def tres():
             print('|Programa cerrado')
             print('----------------------------------------')
             exit()
 
 def registrar_usuario(usuario):
-    with open("users.txt", "a") as file:
-        print("|Ingrese su contraseña: ")
-        contraseña = input(">>  ")
-        print("|Repita su contraseña: ")
-        contraseña2 = input(">>  ")
-        if contraseña != contraseña2:
-            print("|Las contraseñas no coinciden, intente nuevamente")
-            registrar_usuario(usuario)
-        else:
-            contraseña_hasheada = hashear_contraseña(contraseña)
-            file.write(f"{usuario},{contraseña_hasheada}\n")
+    print("|Ingrese su contraseña: ")
+    contraseña = input(">>  ")
+    print("|Repita su contraseña: ")
+    contraseña2 = input(">>  ")
+    if contraseña != contraseña2:
+        print("|Las contraseñas no coinciden, intente nuevamente")
+        registrar_usuario(usuario)
+    else:
+        with open("users.txt", "a") as file:    
+            contraseña_hashear = hashear_contraseña(contraseña)
+            file.write(f"{usuario},{contraseña_hashear}\n")
             file.close()
             print("|Usuario creado con exito")
-
+            consola()
 
 def dos():
     print("|Creando nuevo usuario...")
     print("|Ingrese su nombre de usuario:")
     usuario = str(input('|>>  ').strip())
     with open("users.txt", "r") as file:
+        usuario_almacenado = []
         for line in file.readlines():
             usuario_almacenado, contraseña_almacenada = line.strip().split(",")
-            if usuario_almacenado == usuario:
-                print("|El usuario ya existe, pruebe otro")
-                dos()
-            else:
-                registrar_usuario(usuario)
-               
+        if usuario_almacenado == usuario:
+            print("|El usuario ya existe, pruebe otro")
+            file.close()
+            dos()
+        else:
+            file.close()
+            registrar_usuario(usuario)
+
+def uno():
+    print("|Ingrese su nombre de usuario: ")
+    usuario_ingresado = str(input('|>>  ').strip())
+    print("|Ingrese su contraseña: ")
+    contraseña_ingresadaa = input(">>  ")
+    if verificar_contraseña(contraseña_ingresadaa, usuario_ingresado) == True:
+        print("|Bienvenido " + usuario_ingresado)
+        print('----------------------------------------')
+    else:
+        print("|Contraseña incorrecta")
+        uno() #Ojo con esto puedo estar en un loop infinito. Lo mismo con las otras opciones chequear.
+
 def consola():
     print('----------------------------------------')
     print("|Bienvenido al simulador 'Restaurant City'")
@@ -52,6 +69,8 @@ def consola():
 
     if opcion == "2":
         dos()
+    if opcion == "1":
+        uno()
         
     
 
--- Hasheo.py
+++ Hasheo.py
@@ -1,15 +1,13 @@
 import hashlib
 import os
 
-def hashear_contraseña(contraseña):
-    inicio = os.urandom(16)
-    contraseña_hasheada = hashlib.pbkdf2_hmac("sha256", contraseña.encode(), inicio, 100000)
-    return inicio, contraseña_hasheada
+inicio = "adminasdsdfaergnlnqj!@#r412354ejfoiasdnvjklzdkvna//as".encode() #Hago esto porque no me salio generar un inicio aleatorio y almacenarlo para despues usarlo. sal = os.urandom(16)
 
-def verificar_contraseña(contraseña_almacenada, contraseña_ingresada, usuario_ingresado):
-    with open("inicio.txt", "r") as file:
-        for line in file.readlines():
-            inicio = line
+def hashear_contraseña(contraseña):
+    contraseña_hasheada = hashlib.pbkdf2_hmac("sha256", contraseña.encode(), inicio, 100000)
+    return contraseña_hasheada
+
+def verificar_contraseña(contraseña_ingresada, usuario_ingresado):
     with open("users.txt", "r") as file:
         for line in file.readlines():
             usuario_almacenado, contraseña_almacenada = line.strip().split(",")
