fcef6b664d25adcfc905751c2a678be412214a14
apaolina <128314554+apaolina@users.noreply.github.com>
2023-04-29 18:52:21 -0300

    Cambios

--- GUI.py
+++ GUI.py
@@ -1,21 +1,59 @@
 from Hasheo import *
 
+def tres():
+            print('|Programa cerrado')
+            print('----------------------------------------')
+            exit()
+
+def registrar_usuario(usuario):
+    with open("users.txt", "a") as file:
+        print("|Ingrese su contraseña: ")
+        contraseña = input(">>  ")
+        print("|Repita su contraseña: ")
+        contraseña2 = input(">>  ")
+        if contraseña != contraseña2:
+            print("|Las contraseñas no coinciden, intente nuevamente")
+            registrar_usuario(usuario)
+        else:
+            contraseña_hasheada = hashear_contraseña(contraseña)
+            file.write(f"{usuario},{contraseña_hasheada}\n")
+            file.close()
+            print("|Usuario creado con exito")
+
+
+def dos():
+    print("|Creando nuevo usuario...")
+    print("|Ingrese su nombre de usuario:")
+    usuario = str(input('|>>  ').strip())
+    with open("users.txt", "r") as file:
+        for line in file.readlines():
+            usuario_almacenado, contraseña_almacenada = line.strip().split(",")
+            if usuario_almacenado == usuario:
+                print("|El usuario ya existe, pruebe otro")
+                dos()
+            else:
+                registrar_usuario(usuario)
+               
 def consola():
     print('----------------------------------------')
-    print("Bienvenido al simulador 'Restaurant City'")
-    print("1 - Iniciar sesion")
-    print("2 - Registrar nuevo usuario")
-    print("3 - Salir")
-    print("Ingrese el numero de la opcion que desea realizar:")
-    opcion = input('>>>>>')
+    print("|Bienvenido al simulador 'Restaurant City'")
+    print("|1 - Iniciar sesion")
+    print("|2 - Registrar nuevo usuario")
+    print("|3 - Salir")
+    print("|Ingrese el numero de la opcion que desea realizar:")
+    opcion = input('|>>  ')
     if opcion == "1" or opcion == "2" or opcion == "3":
         pass
     else:
-        print("Ingrese una opcion valida")
+        print("|Ingrese una opcion valida")
         consola()
     if opcion == "3":
-        print('Programa cerrado')
-        print('----------------------------------------')
-        exit()
+        tres()
+
+    if opcion == "2":
+        dos()
+        
+    
+
 
 consola()
--- prueba.py
+++ prueba.py
@@ -13,37 +13,4 @@
 
 # print(mesa.getEstado)
 
-import hashlib
-import os
 
-def hash_password(password):
-    # Generar una sal aleatoria
-    salt = os.urandom(16)
-    # Aplicar el hash a la contraseña junto con la sal
-    hashed_password = hashlib.pbkdf2_hmac("sha256", password.encode(), salt, 100000)
-    # Devolver la sal y el hash de la contraseña
-    return salt + hashed_password
-
-def verify_password(stored_password, entered_password):
-    # Obtener la sal y el hash de la contraseña almacenada
-    salt = stored_password[:16]
-    stored_hash = stored_password[16:]
-    # Aplicar el hash a la contraseña ingresada junto con la sal
-    entered_hash = hashlib.pbkdf2_hmac("sha256", entered_password.encode(), salt, 100000)
-    # Comparar los hashes
-    return entered_hash == stored_hash
-
-# Ejemplo de cómo verificar si la contraseña ingresada es correcta
-
-# Obtener la contraseña almacenada del usuario (que incluye la sal y el hash de la contraseña)
-stored_password = hash_password("mypassword123")
-print(stored_password)
-stored_password1 = hash_password("mypassword123")
-print(stored_password1)
-# Obtener la contraseña ingresada por el usuario
-entered_password = "mypassword123"
-# Verificar si la contraseña ingresada es correcta
-if verify_password(stored_password, entered_password):
-    print("Contraseña correcta!")
-else:
-    print("Contraseña incorrecta.")
