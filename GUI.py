d3218d66de6a7a8a1ef5aaca20441710c3137d64
apaolina <128314554+apaolina@users.noreply.github.com>
2023-04-29 18:25:40 -0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,14 +1,21 @@
 from Hasheo import *
 
 def consola():
+    print('----------------------------------------')
     print("Bienvenido al simulador 'Restaurant City'")
     print("1 - Iniciar sesion")
     print("2 - Registrar nuevo usuario")
+    print("3 - Salir")
     print("Ingrese el numero de la opcion que desea realizar:")
-    opcion = input()
-    if opcion == int:
+    opcion = input('>>>>>')
+    if opcion == "1" or opcion == "2" or opcion == "3":
         pass
     else:
         print("Ingrese una opcion valida")
         consola()
+    if opcion == "3":
+        print('Programa cerrado')
+        print('----------------------------------------')
+        exit()
 
+consola()
