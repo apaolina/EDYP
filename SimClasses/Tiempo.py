b470f661679593e930ca31910c8a6e29f1414eff
jcarzalo <jcarzalo@itba.edu.ar>
2023-05-02 11:17:59 -0300

    Ahora si, cambio de fijos por dist normal en persona

--- SimClasses/Tiempo.py
+++ SimClasses/Tiempo.py
@@ -3,3 +3,6 @@ import numpy as np
 # Se le debe ingresar un valor en segundos 
 def tiempo_aleatorio(lamda: int) -> int:
     return np.random.poisson(lamda, 1)
+
+def tiempo_aleatorio_normal(mu: int, sigma: int) -> int:
+    return np.random.normal(mu, sigma, 1)
