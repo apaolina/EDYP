c0a28a76b1249c77666f9036381a3ee9ad2d2ed9
JoPalermo <129459065+JoPalermo@users.noreply.github.com>
2023-05-18 18:57:26 -0300

    Correcciones de la parte 1

--- GUIClass.py
+++ GUIClass.py
@@ -32,7 +32,6 @@ class Simulador:
         contraseña2 = input("|>>  ")
         if contraseña != contraseña2:
             print("|Las contraseñas no coinciden, intente nuevamente")
-            time.sleep(2)
             self.registrar_usuario(usuario)
         else:  
             try:
@@ -45,10 +44,8 @@ class Simulador:
                     contraseña_hashear = hashear_contraseña(contraseña)
                     file.write(f"{usuario},{contraseña_hashear}\n")
                     file.close()
-                    time.sleep(2)
                     print('|')
                     print("|Usuario creado con exito")
-                    time.sleep(2)
                     self.consola()
                     
             except FileNotFoundError:
@@ -63,7 +60,6 @@ class Simulador:
     def dos(self):
         print('|')
         print("|Creando nuevo usuario...")
-        time.sleep(2)
         print("|Ingrese su nombre de usuario:")
         usuario = str(input('|>>  ').strip())
         if not os.path.exists("users.txt"):
@@ -79,7 +75,6 @@ class Simulador:
                 if usuarios_almacenados[i] == usuario:
                     print("|El usuario ya existe, pruebe otro")
                     file.close()
-                    time.sleep(2)
                     self.consola()
             file.close()
             self.registrar_usuario(usuario)
@@ -90,7 +85,6 @@ class Simulador:
             file.close()
             print('|')
             print("|Resultados guardados con exito")
-            time.sleep(2)
             exit()
 
     def resultado_simulacion(self, start, end):
@@ -114,7 +108,6 @@ class Simulador:
         if not tiempoSimulacion.isdigit():
             print('|')
             print("|El tiempo de simulación debe ser un número entero. Por favor, ingrese un número entero.")
-            time.sleep(3)
             self.empezar_simulacion()
         tiempoSimulacion = int(tiempoSimulacion)
         self.tiempo = tiempoSimulacion
@@ -130,7 +123,6 @@ class Simulador:
         if not numero_cocineros.isdigit():
             print('|')
             print("|El numero de cocineros debe ser un numero entero. Ingrese nuevamente. ")
-            time.sleep(2)
             self.crear_empleados()
         numero_cocineros = int(numero_cocineros)
         self.cantidad_cocineros = numero_cocineros
@@ -144,7 +136,6 @@ class Simulador:
         print("|Nombres de los cocineros creados:")
         for nombre in nombres_Cocineros:
             print('|')
-            time.sleep(1)
             print(f"|El cocinero {nombre} ha sido creado correctamente")
         
         print('|')    
@@ -153,7 +144,6 @@ class Simulador:
         if not numero_mozos.isdigit():
             print('|')
             print("|El numero de mozos debe ser un numero entero. Ingrese  todo nuevamente. ")
-            time.sleep(2)
             self.crear_empleados()
         numero_mozos = int(numero_mozos)
         nombres_Mozos = []
@@ -166,7 +156,6 @@ class Simulador:
         print("|Ha creado " + str(numero_mozos) + " mozos con exito")
         print("|Nombres de los mozos creados:")
         for nombre in nombres_Mozos:
-            time.sleep(1)
             print('|')
             print(f"|El mozo {nombre} ha sido creado correctamente")
         self.empezar_simulacion()
@@ -174,29 +163,24 @@ class Simulador:
     def crear_mesas(self, usuario_ingresado):
         print('|')
         print("|Bienvenido al simulador 'Restaurant City' " + usuario_ingresado)
-        time.sleep(1)
         print('|')
         print("|¿Cuántas mesas desea agregar a la simulación?")
         mesas = input('|>>  ')
         if not mesas.isdigit():
             print('|')
             print("|El numero de mesas debe ser un numero entero. Ingrese nuevamente. ")
-            time.sleep(2)
             self.crear_mesas(usuario_ingresado)
         
         mesas = int(mesas)
         self.cantidad_mesas = mesas
         print('|')
         print("|¿Desea que todas las mesas tengan la misma capacidad?")
-        time.sleep(1)
         print("|1 - Si")
-        time.sleep(1)
         print("|2 - No")
         respuesta = input('|>>  ')
         if respuesta != "1" and respuesta != "2":
             print('|')
             print("|La respuesta debe ser 1 o 2. Ingrese nuevamente. ")
-            time.sleep(2)
             self.crear_mesas(usuario_ingresado)
         if respuesta == "1":
             print("|Ingrese la capacidad de las mesas:")
@@ -204,7 +188,6 @@ class Simulador:
             if not capacidad_mesas.isdigit():
                 print('|')
                 print("|La capacidad de las mesas debe ser un numero entero. Ingrese nuevamente. ")
-                time.sleep(2)
                 self.crear_mesas(usuario_ingresado)
             
             capacidad_mesas = int(capacidad_mesas)
@@ -212,7 +195,6 @@ class Simulador:
                 instance.mesaManager.crearMesa(capacidad_mesas)
             print('|')
             print("|Ha creado " + str(mesas) + " mesas de " + str(capacidad_mesas) + " personas cada una")
-            time.sleep(1)
             self.crear_empleados()
         if respuesta == "2":
             for i in range(1, mesas+1):
@@ -221,12 +203,10 @@ class Simulador:
                 if not capacidad_mesas.isdigit():
                     print('|')
                     print("|La capacidad de las mesas debe ser un numero entero. Ingrese nuevamente. ")
-                    time.sleep(2)
                     self.crear_mesas(usuario_ingresado)
         
                 capacidad_mesas = int(capacidad_mesas)
                 instance.mesaManager.crearMesa(capacidad_mesas)
-            time.sleep(1)
             print('|')
             print("|Ha creado " + str(mesas) + " mesas de diferentes capacidades")
             self.crear_empleados()
@@ -237,13 +217,11 @@ class Simulador:
         self.nombre = nombre_simulacion
         print('|')
         print('|')
-        time.sleep(1)
         print("|---------------------------------")
         print("|||||||||  " + nombre_simulacion + "  |||||||||")
         print("|---------------------------------")
         print('|')
         
-        time.sleep(3)
         self.crear_mesas(usuario_ingresado)
 
     def uno(self):
@@ -262,7 +240,6 @@ class Simulador:
             if existe == False:
                 print('|')
                 print("|El usuario no existe, pruebe otro")
-                time.sleep(2)
                 file.close()
                 self.consola()
             file.close()
@@ -271,30 +248,22 @@ class Simulador:
         contraseña_ingresada = input("|>>  ")
         verificador = verificar_contraseña(contraseña_ingresada, usuario_ingresado)
         if verificador == True:
-            time.sleep(1)
             print('----------------------------------------')
             print("|Bienvenido " + usuario_ingresado)
             print('----------------------------------------')
-            time.sleep(1)
             self.nombre_simulacion(usuario_ingresado)
         if verificador == False:
             print('|')
             print("|Contraseña incorrecta")
-            time.sleep(2)
             self.consola()
             
     def consola(self):
         print('----------------------------------------')
         print("|Bienvenido al simulador 'Restaurant City'")
-        time.sleep(1)
         print("|1 - Iniciar sesion")
-        time.sleep(1)
         print("|2 - Registrar nuevo usuario")
-        time.sleep(1)
         print("|3 - Salir")
-        time.sleep(1)
         print("|4 - Resultados históricos (Coming soon))")
-        time.sleep(1)
         print('|')
         print("|Ingrese el numero de la opcion que desea realizar:")
         opcion = input('|>>  ')
@@ -303,7 +272,6 @@ class Simulador:
         else:
             print('|')
             print("|Ingrese una opcion valida")
-            time.sleep(2)
             self.consola()
         if opcion == "3":
             self.tres()
